f99638e216ae009528482f1445a1d96052d534f7
CengizhanParlak <cengizhan.parlak@hotmail.com>
2023-07-16 04:48:38 +0300

    update: pages

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -8,6 +8,14 @@ const rerender = () => {
   // https://github.com/i18next/jquery-i18next#usage-of-selector-function
   $("body").localize();
   $("title").text($.t("index.welcomeTo"));
+  var inputElement = document.getElementById("nameInp");
+  inputElement.placeholder = $.t("contact.name");
+  var inputElement = document.getElementById("emailInp");
+  inputElement.placeholder = $.t("contact.email");
+  var inputElement = document.getElementById("phoneInp");
+  inputElement.placeholder = $.t("contact.phone");
+  var inputElement = document.getElementById("messageInp");
+  inputElement.placeholder = $.t("contact.message");
 };
 
 $(function () {
@@ -40,6 +48,50 @@ $(function () {
                 contact: "İLETİŞİM",
                 language: "DİL",
               },
+              contact: {
+                sendYourProblem: "Sorununuz için bizimle iletişime geçin.",
+                explanation1:
+                  "Teknenizle ilgili bir sorununuz mu var? TescoMarine; bakım, onarım, tamirat; sistem yenileme; gözlem; iç dış döşeme tasarım ve dekorasyon hizmetleriyle birlikte her türlü boya uygulamaları ile sizlerin hizmetinde.",
+                explanation2:
+                  "Formu doldurarak bizimle iletişime geçebilirsiniz. Sorununuz hakkında bilgi vermeniz bize yardımcı olacaktır. En kısa sürede sizinle iletişime geçeceğiz.",
+                form: "İletişim Formu",
+                fillAllAreas:
+                  "Tüm bilgilerinizi eksiksiz ve doğru bir şekilde doldurun.",
+                giveDetails:
+                  "Lütfen sorunu olabildiğince detaylı anlatın. En kısa sürede sizinle iletişime geçeceğiz.",
+                name: "Adınız",
+                email: "E-posta Adresiniz",
+                phone: "Telefon Numaranız",
+                message: "Mesajınız",
+                errorSending:
+                  "Mesajınız gönderilirken bir hata oluştu. Lütfen tekrar deneyin.",
+                tyForSending:
+                  "Bizi tercih ettiğiniz için teşekkür ederiz. En kısa sürede sizinle iletişime geçeceğiz.",
+              },
+              services: {
+                services: "Hizmetlerimiz",
+                servicesExpShort:
+                  "TescoMarine olarak tekne bakım hizmetleri, tekne onarım hizmetleri, yenileme hizmetleri; proje tasarımı, imalat ve montaj hizmetleri, hidrolik sistem arge tasarımı ve hidrolik sistem bakım hizmetleri ile satış; şişme bot ve motor bakım ve onarımı; dekorasyon ve boya hizmetleri sunmaktayız.",
+                boatJobs: "Tekne ve Yat İşleri",
+                explanation:
+                  "Deneyimli ve uzman kadromuzla birlikte, teknenizdeki tüm sorunlar için sizlere hizmet  Uygulamalarımızdan bazıları:",
+                repair: "Onarım İşlemleri",
+                repairExpShort:
+                  "Rutin veya acil onarım; elektrik, elektronik, mekanik, motor onarımlarıyla birlikte şişme bot ve motorlarının onarımları.",
+                maintenance: "Bakım İşlemleri",
+                maintenanceExpShort:
+                  "Tekne bakım işlemleri ve tekne bakımı ile ilgili hizmetler. Elektrik ve elektronik bakımları, dizel ve benzin motor bakımları, hidrolik sistemlerin bakımları.",
+                project: "Proje Tasarım ve Montaj",
+                projectExpShort:
+                  "Tekne ve yatlarınız için proje tasarım ve montaj hizmetleri. Elektrik ve elektronik proje tasarımı, imalatı ve montajı; hidrolik ve mekanik proje tasarımı, imalat ve montajıyla sistem arge tasarım.",
+                renovation: "Yenileme Projeleri",
+                renovationExpShort:
+                  "Tekne yenileme projeleri ve yat yenileme projeleri ile ilgili hizmetler. Hidrolik sistem yenileme; modifikasyon ve sistem yenileme projeleri.",
+              },
+              misc: {
+                loading: "Yükleniyor...",
+                send: "Gönder",
+              },
             },
           },
           en: {
@@ -57,6 +109,46 @@ $(function () {
                 blog: "BLOG",
                 contact: "CONTACT",
                 language: "LANGUAGE",
+              },
+              contact: {
+                sendYourProblem: "Contact us for your problem.",
+                explanation1: `Do you have a problem with your boat? TescoMarine is at your service with maintenance, repair, renovation, system renewal, observation, interior and exterior upholstery design and decoration services, as well as all kinds of paint applications.`,
+                explanation2: `You can contact us by filling out the form. It will be helpful for us if you provide information about your problem. We will contact you as soon as possible.`,
+                form: "Contact Form",
+                fillAllAreas: "Fill in all your information correctly.",
+                giveDetails:
+                  "Please describe the problem in as much detail as possible. We will contact you as soon as possible.",
+                name: "Name",
+                email: "E-mail Address",
+                phone: "Phone Number",
+                message: "Message",
+                errorSending:
+                  "An error occurred while sending your message. Please try again.",
+                tyForSending:
+                  "Thank you for choosing us. We will contact you as soon as possible.",
+              },
+              services: {
+                services: "Servicess",
+                servicesExpShort:
+                  "As TescoMarine, we offer boat maintenance services, boat repair services, renovation services; project design, manufacturing and installation services, hydraulic system R&D design and hydraulic system maintenance services and sales; inflatable boat and engine maintenance and repair; decoration and painting services.",
+                boatJobs: "Boat and Yacht Jobs",
+                explanation:
+                  "With our experienced and expert team, we provide you with services for all the problems on your boat. Some of our applications:",
+                repair: "Repair",
+                repairExpShort:
+                  "Routine or emergency repair; electrical, electronic, mechanical, engine repairs along with inflatable boat and engine repairs.",
+                maintenance: "Maintenance",
+                maintenanceExpShort: `Boat maintenance and services related to boat maintenance. Electrical and electronic maintenance, diesel and gasoline engine maintenance, maintenance of hydraulic systems.`,
+                project: "Project Design and Installation",
+                projectExpShort:
+                  "Project design and installation services for your boats and yachts. Electrical and electronic project design, manufacturing and installation; hydraulic and mechanical project design, manufacturing and installation with system R&D design.",
+                renovation: "Renovation Projects",
+                renovationExpShort:
+                  "Services related to boat renovation projects and yacht renovation projects. Hydraulic system renewal; modification and system renewal projects.",
+              },
+              misc: {
+                loading: "Loading...",
+                send: "Send",
               },
             },
           },
